c117abdf80ba2b606518ee6b259e4abc472d7483
Alexey Yarov <alexxd91@gmail.com>
2021-06-17 15:26:59 +0300

    Initial Commit

---
+++ Where Was I/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Where Was I
+//
+//  Created by Alexey Yarov on 17.06.2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
